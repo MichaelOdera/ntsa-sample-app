2f4fc8ac88227f36939892d8e9ca4ba7aa855463
MichaelOdera <michogelira@gmail.com>
2020-06-01 02:53:17 +0300

    added a click listener to the launcher button when the it clicked such that it leads to the MainActivity Activity when clicked

--- app/src/main/java/com/example/drawers/LauncherActivity.java
+++ app/src/main/java/com/example/drawers/LauncherActivity.java
@@ -1,13 +1,38 @@
 package com.example.drawers;
 
 import androidx.appcompat.app.AppCompatActivity;
-import android.os.Bundle;
 
-public class LauncherActivity extends AppCompatActivity {
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class LauncherActivity extends AppCompatActivity implements View.OnClickListener{
+
+    @BindView(R.id.launcherButton)
+    Button mLauncherButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_launcher);
+
+        ButterKnife.bind(this);
+
+        mLauncherButton.setOnClickListener(this);
+
+
+    }
+
+    @Override
+    public void onClick(View view) {
+        if(view == mLauncherButton) {
+            Intent launcherIntent = new Intent(LauncherActivity.this, MainActivity.class);
+            startActivity(launcherIntent);
+        }
+
     }
 }
