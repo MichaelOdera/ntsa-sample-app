7587bbfbbece61ba607661ecb3fd9e3becef5599
MichaelOdera <michogelira@gmail.com>
2020-06-01 02:47:33 +0300

    added the progressbar functionality on loading the url page of the Ecitizen to the EcitizenActivity.java file

--- app/src/main/java/com/example/drawers/EcitizenActivity.java
+++ app/src/main/java/com/example/drawers/EcitizenActivity.java
@@ -2,8 +2,12 @@ package com.example.drawers;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.view.View;
 import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.ProgressBar;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -11,6 +15,7 @@ import butterknife.ButterKnife;
 public class EcitizenActivity extends AppCompatActivity {
 
     @BindView(R.id.eCitizenWebView) WebView mCitizenWebView;
+    @BindView(R.id.progressBar) ProgressBar mProgressBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,6 +23,25 @@ public class EcitizenActivity extends AppCompatActivity {
         setContentView(R.layout.activity_ecitizen);
         ButterKnife.bind(this);
 
+        mCitizenWebView.setWebViewClient(new WebViewClient());
+
         mCitizenWebView.loadUrl("https://ntsa.ecitizen.go.ke/");
+    }
+
+    public class WebViewClient extends android.webkit.WebViewClient{
+        @Override
+        public void onPageStarted(WebView view, String url, Bitmap favicon) {
+            super.onPageStarted(view, url, favicon);
+        }
+        @Override
+        public boolean shouldOverrideUrlLoading(WebView view, String url) {
+            view.loadUrl(url);
+            return true;
+        }
+        @Override
+        public void onPageFinished(WebView view, String url) {
+            super.onPageFinished(view, url);
+            mProgressBar.setVisibility(View.GONE);
+        }
     }
 }
