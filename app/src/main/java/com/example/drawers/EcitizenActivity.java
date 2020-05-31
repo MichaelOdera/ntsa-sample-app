63f2c3306a2d488895c9af7876c9e36ed775b726
MichaelOdera <michogelira@gmail.com>
2020-06-01 01:10:30 +0300

    generated an EcitizenActivity.java file that is to hold the url of the EcitizenWebView

--- app/src/main/java/com/example/drawers/EcitizenActivity.java
+++ app/src/main/java/com/example/drawers/EcitizenActivity.java
@@ -1,13 +1,23 @@
 package com.example.drawers;
 
 import androidx.appcompat.app.AppCompatActivity;
+
 import android.os.Bundle;
+import android.webkit.WebView;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class EcitizenActivity extends AppCompatActivity {
+
+    @BindView(R.id.eCitizenWebView) WebView mCitizenWebView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ecitizen);
+        ButterKnife.bind(this);
+
+        mCitizenWebView.loadUrl("https://tims.ntsa.go.ke/login_csp.jsp");
     }
 }
