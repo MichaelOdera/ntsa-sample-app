940f7313aa15dcf953790f6805a61e526484fe70
MichaelOdera <michogelira@gmail.com>
2020-06-01 02:51:38 +0300

    added the functionality for the progressbar to the KplcActivty.java file and also included the url for the kplc tokens when the page is supposed to load at the click of the fab button

--- app/src/main/java/com/example/drawers/KplcActivity.java
+++ app/src/main/java/com/example/drawers/KplcActivity.java
@@ -1,13 +1,44 @@
 package com.example.drawers;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.view.View;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.ProgressBar;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class KplcActivity extends AppCompatActivity {
+    @BindView(R.id.kplcWebView) WebView mKplcWebView;
+    @BindView(R.id.progressBar) ProgressBar mProgressBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_kplc);
+        ButterKnife.bind(this);
+
+        mKplcWebView.setWebViewClient(new WebViewClient());
+        mKplcWebView.loadUrl("https://kplc.co.ke/category/view/50/power-interruptions");
+    }
+    public class WebViewClient extends android.webkit.WebViewClient{
+        @Override
+        public void onPageStarted(WebView view, String url, Bitmap favicon) {
+            super.onPageStarted(view, url, favicon);
+        }
+        @Override
+        public boolean shouldOverrideUrlLoading(WebView view, String url) {
+            view.loadUrl(url);
+            return true;
+        }
+        @Override
+        public void onPageFinished(WebView view, String url) {
+            super.onPageFinished(view, url);
+            mProgressBar.setVisibility(View.GONE);
+        }
     }
 }
