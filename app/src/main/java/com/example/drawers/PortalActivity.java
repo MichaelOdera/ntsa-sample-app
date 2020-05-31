0cbebb5f4ed20d4e88824104dd1bf56535f94e33
MichaelOdera <michogelira@gmail.com>
2020-06-01 01:14:08 +0300

    added a PortalActivity.java Activity to hold the webview of the ntsa portal when the button for transition is clicked

--- app/src/main/java/com/example/drawers/PortalActivity.java
+++ app/src/main/java/com/example/drawers/PortalActivity.java
@@ -3,12 +3,21 @@ package com.example.drawers;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.webkit.WebView;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class PortalActivity extends AppCompatActivity {
+    @BindView(R.id.portalWebView)
+    WebView mPortalWebView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_portal);
+        ButterKnife.bind(this);
+
+        mPortalWebView.loadUrl("https://tims.ntsa.go.ke/login_csp.jsp");
     }
 }
