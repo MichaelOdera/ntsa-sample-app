e1d92721a2b8d177130690bfd037eb6f5616c6df
MichaelOdera <michogelira@gmail.com>
2020-06-01 04:01:01 +0300

    made the main textView of the Launcher Activity clickable

--- app/src/main/java/com/example/drawers/LauncherActivity.java
+++ app/src/main/java/com/example/drawers/LauncherActivity.java
@@ -6,6 +6,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -14,6 +15,8 @@ public class LauncherActivity extends AppCompatActivity implements View.OnClickL
 
     @BindView(R.id.launcherButton)
     Button mLauncherButton;
+    @BindView(R.id.kplcTextView)
+    TextView mTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,6 +26,7 @@ public class LauncherActivity extends AppCompatActivity implements View.OnClickL
         ButterKnife.bind(this);
 
         mLauncherButton.setOnClickListener(this);
+        mTextView.setOnClickListener(this);
 
 
     }
@@ -32,6 +36,10 @@ public class LauncherActivity extends AppCompatActivity implements View.OnClickL
         if(view == mLauncherButton) {
             Intent launcherIntent = new Intent(LauncherActivity.this, MainActivity.class);
             startActivity(launcherIntent);
+        }
+        if(view == mTextView) {
+            Intent kplcIntent = new Intent(LauncherActivity.this, KplcActivity.class);
+            startActivity(kplcIntent);
         }
 
     }
