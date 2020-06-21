dfc2bc9391fb04ed2103c562de3816a12ebad554
MichaelOdera <michogelira@gmail.com>
2020-06-21 21:31:26 +0300

    added the firebase dependencies to enable firebase OTP login

---
+++ app/src/main/java/com/example/drawers/ui/authentication/LoginActivity.java
@@ -0,0 +1,14 @@
+package com.example.drawers.ui.login_signup;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
---
+++ app/src/main/java/com/example/drawers/ui/authentication/RegistrationActivity.java
@@ -0,0 +1,13 @@
+package com.example.drawers.ui.authentication;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class RegistrationActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_registration);
+    }
+}
