02327436de6823a969e7d8646a8e6d906b9d3ac1
MichaelOdera <michogelira@gmail.com>
2020-06-21 21:40:28 +0300

    created a LoginActivity.java activity with a corresponding layout xml file to allow for a user to login when in need of doing so

--- app/src/main/java/com/example/drawers/ui/authentication/LoginActivity.java
+++ app/src/main/java/com/example/drawers/ui/authentication/LoginActivity.java
@@ -1,14 +1,22 @@
-package com.example.drawers.ui.login_signup;
+package com.example.drawers.ui.authentication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.example.drawers.R;
+import com.google.firebase.auth.FirebaseAuth;
+
 public class LoginActivity extends AppCompatActivity {
+
+    private FirebaseAuth mAuth;
+    private FirebaseAuth.AuthStateListener mAuthListener;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+
     }
 }
